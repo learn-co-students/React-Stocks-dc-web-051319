e364eb48e88dadc3b261db6e7bbc3573600b5988
Salsa-Dude <arias.joseph.a@gmail.com>
2019-07-21 22:18:46 -0400

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,41 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+
+  filterStock = () => {
+    let filterStock;
+
+    if(this.props.filterBy === "None") {
+      filterStock = this.props.allStocks
+    }
+    else if (this.props.filterBy === "Finance") {
+      filterStock = this.props.allStocks.filter(stock => {
+        return stock.type === "Finance"
+      })
+    }
+    else if (this.props.filterBy === "Sportswear") {
+      filterStock = this.props.allStocks.filter(stock => {
+        return stock.type === "Sportswear"
+      })
+    }
+    else if (this.props.filterBy === "Tech") {
+      filterStock = this.props.allStocks.filter(stock => {
+        return stock.type === "Tech"
+      })
+    }
+
+    return filterStock
+  }
+
   render() {
+
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.filterStock().map(stock => {
+            return <Stock key={stock.id} stock={stock} handleClick={this.props.handleClick} />
+          })
         }
       </div>
     );
